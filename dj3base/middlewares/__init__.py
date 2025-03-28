6b0319ff11b08d4cd443f713d78ed0ef418deed5
three-kinds <3179158552@qq.com>
2025-03-28 08:35:39 +0800

    After ruff check.

--- dj3base/middlewares/__init__.py
+++ dj3base/middlewares/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 from .error_handling_middleware import ErrorHandlingMiddleware
+
+__all__ = ["ErrorHandlingMiddleware"]
--- dj3base/views/__init__.py
+++ dj3base/views/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 from .post_json_view import PostJsonView
+
+__all__ = ["PostJsonView"]
